e48430b2d34446d7397cd7d62f7010ab158dee1f
Mustafa Akgilli <mustafaakgilli@gmail.com>
2014-07-10 13:57:40 +0300

    Hour Enum
    produces hours

---
+++ src/cinema/model/Hour.java
@@ -0,0 +1,24 @@
+package cinema.model;
+
+public enum Hour {
+ 
+TEN(10),TWELVE(12),FOURTEEN(14),SÝXTEEEN(16),EIGHTEEN(18),TWENTY(20),TWENTYTWO(22);
+
+public int hour;
+
+/*
+ * @Author Mustafa Akgilli
+ * @return
+ * @subject this class produce hours
+ */
+private Hour (int hour){
+	this.hour=hour;
+}
+
+public int getHour() {
+	return hour;
+}
+
+ 
+
+}
